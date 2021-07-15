d476c13e74f9ef256ddeb92ed573a165337c80d8
Jose Cavalcanti <josecavalcanti@Joses-Air.lan>
2021-07-15 17:43:28 -0300

    report return ok

--- app/src/main/java/com/example/vigsafeversion01/DbManager.java
+++ app/src/main/java/com/example/vigsafeversion01/DbManager.java
@@ -2,12 +2,18 @@ package com.example.vigsafeversion01;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 
 public class DbManager extends SQLiteOpenHelper {
     private static final String dbname="report.db";
+    private static final String TABLENAME = "report";
+    private static final String TAG = "TAG";
 
     public DbManager(Context context) {
         super(context, dbname, null, 1);
@@ -15,14 +21,14 @@ public class DbManager extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String qry = "create table report (id integer primary key autoincrement, productType text, temperature text, date text)";
+        String qry = "create table " + TABLENAME +  "(id integer primary key autoincrement, productType, temperature, date)";
         db.execSQL(qry);
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS report" );
+        db.execSQL("DROP TABLE IF EXISTS " + TABLENAME);
         onCreate(db);
 
     }
@@ -33,7 +39,7 @@ public class DbManager extends SQLiteOpenHelper {
         cv.put("temperature", p2);
         cv.put("date", p3);
 
-        long res = db.insert("report", null, cv);
+        long res = db.insert(TABLENAME, null, cv);
 
         if(res==-1) {
             return "Failed";
@@ -41,4 +47,17 @@ public class DbManager extends SQLiteOpenHelper {
             return "Successfully inserted";
         }
     }
+    public ArrayList<Measure> getData(String date) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String qry = "select * from " + TABLENAME + " where date LIKE '%" + date +"%'";
+
+        Cursor cursor = db.rawQuery(qry, null, null);
+        ArrayList<Measure> result = new ArrayList<Measure>();
+        while (cursor.moveToNext()) {
+            result.add(new Measure(cursor.getString(1), cursor.getString(2), cursor.getString(3)));
+        }
+        return result;
+    }
+
+
 }
--- app/src/main/java/com/example/vigsafeversion01/ReportResponse.java
+++ app/src/main/java/com/example/vigsafeversion01/ReportResponse.java
@@ -2,21 +2,75 @@ package com.example.vigsafeversion01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
 public class ReportResponse extends AppCompatActivity {
+
+    TextView textResponse;
+    EditText textSearch;
+    DbManager obj = null;
+    Button btnSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_response);
 
+        //textResponse = findViewById(R.id.textResponse);
+        textSearch = findViewById(R.id.textSearch);
+        btnSearch = findViewById(R.id.search_button);
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchData();
+            }
+        });
 
     }
+
+    public void searchData() {
+
+        obj = new DbManager(this);
+        ArrayList<Measure> measures= obj.getData(textSearch.getText().toString());
+
+        TableLayout table = (TableLayout)findViewById(R.id.table_report);
+        table.removeAllViews();
+
+        for(Measure measure : measures) {
+            TextView t1 = new TextView(this);
+            TextView t2 = new TextView(this);
+            TextView t3 = new TextView(this);
+
+            t1.setText(measure.getProductType());
+            t2.setText(measure.getTemperature());
+            t3.setText(measure.getDate());
+
+            TableRow row = new TableRow(this);
+            row.addView(t1);
+            row.addView(t2);
+            row.addView(t3);
+
+            table.addView(row);
+        }
+
+
+    }
+
+
+
+
+
 }
